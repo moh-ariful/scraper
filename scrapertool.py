879dcb6e02569a79285d4a8f3f422e713c3f86bc
Moh Ariful <106416095+moh-ariful@users.noreply.github.com>
2023-11-28 16:22:56 +0700

    Update scrapertool.py

--- scrapertool.py
+++ scrapertool.py
@@ -1,68 +1,63 @@
 import requests
 from bs4 import BeautifulSoup
-from urllib.parse import urljoin, urlparse
-from collections import deque
+import urllib3
+from bs4.element import Comment
+from fake_useragent import UserAgent
+import time
 import pyfiglet
 from termcolor import colored
 
-# Buat ASCII art utk teks "SCRAPER-TOOL"
-ascii_banner = pyfiglet.figlet_format("SCRAPER-TOOL")
-print(colored(ascii_banner, 'yellow'))  # Mencetak "SCRAPER-TOOL" dalam huruf kapital berukuran besar dan berwarna kuning
-print(colored("Created by Cak Mad", 'green'))  # Mencetak "Created by Cak Mad" berwarna hijau terang
+# Font standar utk "ContentExtractor", bisa sesuaikan lebarnya jika perlu
+ascii_banner = pyfiglet.figlet_format("ContentExtractor", width=100)
+print(colored(ascii_banner, 'yellow'))
 
-# Konfigurasi permintaan untuk menggunakan Tor
-# Default Tor socks proxy berjalan pada port 9050
-proxies = {
-    'http': 'socks5h://localhost:9050',
-    'https': 'socks5h://localhost:9050'
-}
+# Font lebih kecil utk "by Cak Mad"
+ascii_banner_by = pyfiglet.figlet_format("by Cak Mad", font="digital", width=100)
+print(colored(ascii_banner_by, 'yellow'))
 
-def clean_url(url):
-    # Membersihkan URL dari query string untuk menghindari duplikasi karena parameter yang berubah
-    parsed_url = urlparse(url)
-    cleaned_url = parsed_url.scheme + "://" + parsed_url.netloc + parsed_url.path
-    return cleaned_url
 
-def scrape(site, depth=2, output_file="output.txt"):
-    visited = set()
-    queue = deque([(site, 0)])
+def tag_visible(element):
+    if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
+        return False
+    if isinstance(element, Comment):
+        return False
+    return True
 
-    with open(output_file, "w") as file:
-        while queue:
-            current_url, current_depth = queue.popleft()
-            if current_depth > depth:
-                break
+url = input("Masukkan URL: ")
 
-            try:
-                # Tambahkan argumen proxies ke permintaan
-                page = requests.get(current_url, proxies=proxies)
-                page.raise_for_status()
-                soup = BeautifulSoup(page.text, 'html.parser')
+# Cache User-Agent utk digunakan berulang kali
+ua = UserAgent()
+headers = {"User-Agent": ua.random}
 
-                cleaned_current_url = clean_url(current_url)
-                if cleaned_current_url in visited:
-                    continue
+session = requests.Session()  # Menggunakan session requests
+session.headers = headers
 
-                visited.add(cleaned_current_url)
-                file.write(cleaned_current_url + "\n")
-                print(f"Level: {current_depth}, URL: {cleaned_current_url}")
+try:
+    response = session.get(url, verify=False)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        texts = soup.find_all(string=True)
+        visible_texts = filter(tag_visible, texts)
 
-                for link in soup.find_all('a'):
-                    link_address = link.get('href')
-                    if link_address and not link_address.startswith('#'):
-                        full_link = urljoin(current_url, link_address)
-                        cleaned_full_link = clean_url(full_link)
-                        if cleaned_full_link not in visited:
-                            queue.append((cleaned_full_link, current_depth + 1))
+        print("Konten Teks:")
+        for text in visible_texts:
+            cleaned_text = text.strip()
+            if cleaned_text:
+                print(cleaned_text)
+                time.sleep(1)  # Delay utk setiap 10 teks
 
-            except requests.RequestException as e:
-                print(f"Error pada {current_url}: {e}")
+        print("Gambar:")
+        for image in soup.find_all('img'):
+            if 'src' in image.attrs:
+                print(image['src'])
+                time.sleep(1)  # Delay utk setiap 10 gambar
 
-def main():
-    site = input("Masukkan alamat URL yang ingin discrape: ")
-    scrape(site)
-    print("=========================")
-    print(colored("Thanks, Cak Mad - nextkoding@gmail.com", 'red'))
-
-if __name__ == "__main__":
-    main()
+        print("Link:")
+        for link in soup.find_all('a'):
+            if 'href' in link.attrs:
+                print(link['href'])
+                time.sleep(1)  # Delay utk setiap 10 link
+    else:
+        print(f"Gagal mengakses URL dengan status: {response.status_code}")
+except requests.exceptions.RequestException as e:
+    print(f"Terjadi kesalahan saat mengakses URL: {e}")
